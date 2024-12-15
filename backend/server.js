f11f6a6e7083568602dfbef5eb3109526ab49bb4
Don Hämäläinen <Hamalainen.don@gmail.com>
2024-12-15 23:36:18 +0200

    semantic fix

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,6 @@
 // Dotenv konfiguraatio
 require("dotenv").config();
-
 const express = require("express");
-// const jwt = require("./utils/jwt");
 const { authenticateToken } = require("./utils/jwt");
 
 const authRoutes = require("./routes/auth.routes");
@@ -10,30 +8,11 @@ const userRoutes = require("./routes/user.routes");
 const matchesRoutes = require("./routes/matches.routes");
 
 const app = express();
-
 const port = process.env.PORT || 3000;
 
-let refreshTokens = [];
 // Middleware
+
 app.use(express.json());
-// app.use("/api/reset-token", (req, res) => {
-//   const refreshToken = req.body.token;
-//   if (refreshToken == null) {
-//     return res.sendStatus(401);
-//   }
-//   if (!refreshTokens.includes(refreshToken)) {
-//     return res.sendStatus(403);
-//   }
-//   jwt.verifyToken(
-//     refreshToken,
-//     process.env.REFRESH_TOKEN_SECRET,
-//     (err, user) => {
-//       if (err) return res.sendStatus(403);
-//       const accessToken = jwt.signToken({ username: user.username });
-//       res.json({ accessToken });
-//     }
-//   );
-// });
 // Reitit
 app.use("/api/auth", authRoutes);
 app.use("/api/user", authenticateToken, userRoutes);
